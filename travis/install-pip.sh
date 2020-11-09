3e34843346ba3bc4d9981472a9778b44b7a5f6e2
Roman Feldbauer <sci@feldbauer.org>
2020-11-09 14:28:43 +0100

    switch back to PyTorch stable

--- travis/install-pip.sh
+++ travis/install-pip.sh
@@ -8,11 +8,9 @@ source "$HOME/miniconda/etc/profile.d/conda.sh"
 conda activate test
 hash -r
 
-# Work-around for a bug in PyTorch/MKLDNN on Linux and AVX512 systems:
-# Fetch the pytorch-nightly, until the official release.
 if [[ "$TRAVIS_OS_NAME" == 'linux' ]]; then
-  echo "install and upgrade PyTorch nightly"
-  conda install --yes pytorch cpuonly -c pytorch-nightly
+  echo "install and upgrade PyTorch"
+  conda install --yes pytorch cpuonly -c pytorch
   echo "pip installing required python packages"
   pip install -r requirements/dev.txt
   python -c "import torch; print(f'PyTorch version = {torch.__version__}')"
