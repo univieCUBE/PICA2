aa2a632bab3a165e4fec1299c7405e71037abf9c
Roman Feldbauer <sci@feldbauer.org>
2020-11-09 16:19:14 +0100

    Fix conda & pip calls

--- travis/install-pip.sh
+++ travis/install-pip.sh
@@ -7,13 +7,15 @@ source "$HOME/miniconda/etc/profile.d/conda.sh"
 
 conda activate test
 hash -r
+conda config --set always_yes yes
 
 if [[ "$TRAVIS_OS_NAME" == 'linux' ]]; then
   echo "install and upgrade PyTorch"
-  conda install --yes pytorch cpuonly -c pytorch
+  conda install pytorch cpuonly -c pytorch
   echo "pip installing required python packages"
   pip install -r requirements/dev.txt
-  python -c "import torch; print(f'PyTorch version = {torch.__version__}')"
+  echo "Install sklearn v0.23.0 to match pickled models"
+  pip install scikit-learn==0.23.0
 elif [[ "$TRAVIS_OS_NAME" == 'osx' ]]; then
   echo "install XGboost via conda-forge due to univieCUBE/phenotrex #23"
   conda install -c conda-forge xgboost
